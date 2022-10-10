c27a88f65e95af07a76426ed0362b839d8e22c41
stokrotka24 <hanna.stempniewicz@gmail.com>
2022-10-10 13:31:23 +0200

    [BACK] Add param m to opimized item based algorithm with cosine similarity

--- backend/algorithms/collaborative_filtering/recommendations.py
+++ backend/algorithms/collaborative_filtering/recommendations.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 import numpy as np
@@ -5,13 +6,23 @@ from scipy.sparse import load_npz
 from similarities import cosine_similarity
 
 
-def cosine_item_based(m, weighted_average=True):
-    utility_matrix = load_npz("matrices/utility_matrix.npz")
+def cosine_item_based(utility_matrix, m, weighted_average=True):
+    hotel_similarities = cosine_similarity(utility_matrix, 0).tolil()
+    no_rows = hotel_similarities.shape[0]
+
+    for row_index in range(no_rows):
+        row_data = np.array(hotel_similarities.data[row_index])
+        column_indices = np.array(hotel_similarities.rows[row_index])
+        # get indices of m similar hotels (m+1 because we'll get also the same hotel - similarity(A,A) = 1.0)
+        sorted_data_indices = row_data.argsort()[-(m + 1):]
+        hotel_similarities.data[row_index] = row_data[sorted_data_indices].tolist()
+        hotel_similarities.rows[row_index] = column_indices[sorted_data_indices].tolist()
+
+    hotel_similarities = hotel_similarities.tocsr()
+    hotel_similarities = hotel_similarities.T
 
     utility_matrix_ones = utility_matrix.copy()
     utility_matrix_ones.data = np.ones_like(utility_matrix.data)
-
-    hotel_similarities = cosine_similarity(utility_matrix, 0)
 
     if weighted_average:
         ratings_sum = utility_matrix * hotel_similarities
@@ -26,4 +37,14 @@ def cosine_item_based(m, weighted_average=True):
     return ratings_sum / weights_sum
 
 
-predicted_rating = cosine_item_based(1000, False)
+if __name__ == "__main__":
+    um = load_npz("matrices/utility_matrix.npz")
+    print(um[372, 680])
+    um[372, 680] = 0
+    um.eliminate_zeros()
+    # num_ratings = len(um.getrow(372).data)
+    # print(num_ratings)
+    predicted_rating = cosine_item_based(um, 150, False)
+    print(predicted_rating[372, 680])
+    # print(predicted_rating[372, :][~numpy.isnan(x)])
+
