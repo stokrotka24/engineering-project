0d46fa469e15590304537086161c87cdf30c0f66
stokrotka24 <hanna.stempniewicz@gmail.com>
2022-10-22 09:59:11 +0200

    Test commit after branch changing

--- backend/start_server.sh
+++ backend/start_server.sh
@@ -10,4 +10,4 @@ rm $COMMAND_FILE
 # python3.10 manage.py runserver 0.0.0.0:5000
 # HTTPS:
 python3.10 manage.py runserver_plus --cert-file https/server_public_key.pem --key-file https/server_private_key.pem --keep-meta-shutdown 0.0.0.0:5000
-#python3.10 manage.py crontab remove
+python3.10 manage.py crontab remove
